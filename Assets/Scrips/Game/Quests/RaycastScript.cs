af8e798d7e4a885e28c414a7cd5a246d2ddf0705
Vadim Bondarovich <worldvb2000@gmail.com>
2020-02-13 22:00:27 +0300

    feat: Add base realization of quest

---
+++ Assets/Scrips/Game/Quests/RaycastScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastScript : MonoBehaviour
+{
+    public float rayDistance = 3f;
+    void Update()
+    {
+        var cameras = Camera.allCameras;
+        var ray = Camera.allCameras[0].ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(transform.position, ray.direction * rayDistance);
+        RaycastHit hit;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "CorrectAnswer")
+                {
+                    Debug.Log("Correct answer");
+                    GameObject.Find("QuestionPanel").SetActive(false);
+                    GameObject.Find("Question1").SetActive(false);
+                }
+                else if (hit.transform.tag == "IncorrectAnswer")
+                {
+                    Debug.Log("Incorrect answer");
+                    GameObject.Find("QuestionPanel").SetActive(false);
+                    GameObject.Find("Question1").SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scrips/Game/Quests/TriggerMotor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMotor : MonoBehaviour
+{
+    public GameObject questionPanel;
+    public GameObject masAnswers;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Question is visible");
+            questionPanel.SetActive(true);
+            if (gameObject.name == "Trigger1")
+            {
+                var answers = masAnswers.transform.Find("Question1");
+                foreach (Transform answer in answers)
+                {
+                    answer.GetComponent<BoxCollider>().enabled = true;
+                }
+            }
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
