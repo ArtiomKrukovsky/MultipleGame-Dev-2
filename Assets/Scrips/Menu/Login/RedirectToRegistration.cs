2f047013f1b92d2fbb2f8a56fab7b568dc365bee
Artiom Krukovsky <Artiomgood1@gmail.com>
2020-04-07 23:00:50 +0300

    fix: add "Go to login" btn, translate texts to Russian language, increase quest panel

--- Assets/Scrips/Menu/Login/RedirectToRegistration.cs
+++ Assets/Scrips/Menu/Login/RedirectToRegistration.cs
@@ -1,25 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RedirectToRegistration : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void LoadSceneRegistration()
     {
         SceneManager.LoadScene("Registration");
         Debug.Log("Redirect to scene Registration.");
+    }
+
+    public void RedirectToLoginScene()
+    {
+        SceneManager.LoadScene("Login");
+        Debug.Log("Redirect to scene Login.");
     }
 }
